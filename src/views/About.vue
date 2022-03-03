e49bc5abe517ae3f9a689e9ddaafbeaf18b728a9
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2022-03-03 16:39:20 -0300

    Se agrego los diferentes componentes y logica, para agregar tareas y eliminarlas

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
+  <div class="about pa-6">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+      exercitationem illum magni reprehenderit non et. Modi, sunt. Deleniti
+      minima cupiditate aut modi quod quisquam repellendus delectus a provident,
+      architecto fuga?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+      exercitationem illum magni reprehenderit non et. Modi, sunt. Deleniti
+      minima cupiditate aut modi quod quisquam repellendus delectus a provident,
+      architecto fuga?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+      exercitationem illum magni reprehenderit non et. Modi, sunt. Deleniti
+      minima cupiditate aut modi quod quisquam repellendus delectus a provident,
+      architecto fuga?
+    </p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="home">
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      outlined
+      label="Agregar tarea"
+      append-icon="mdi-plus"
+      class="pa-3"
+      hide-details
+      clearable
+    ></v-text-field>
+
+    <v-list flat class="pt-0">
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+              >
+                {{ task.title }}</v-list-item-title
+              >
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn @click.stop="deleteTask(task.id)" icon>
+                <v-icon color="primary lighten-1">mdi-trash-can-outline</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      newTaskTitle: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false,
+      };
+      this.tasks.push(newTask);
+      this.newTaskTitle = "";
+    },
+    doneTask(id) {
+      let task = this.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+  },
+};
+</script>
